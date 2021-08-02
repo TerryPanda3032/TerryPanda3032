c1b50ac914c292ba47a1cd66a7ec3aca976bed21
TerryPanda3032 <87308076+TerryPanda3032@users.noreply.github.com>
2021-08-02 17:21:10 +0800

    更新 radom1.py

--- radom1.py
+++ radom1.py
@@ -10,20 +10,25 @@ Axis_x=np.linspace(0, 100,30)
 Axis_y=0
 Axis_y=3*Axis_x + 6 + np.random.randn(30) * 7
 print(Axis_x,Axis_y)
-plt.scatter(Axis_x,Axis_y)
+plt.plot(Axis_x,Axis_y)
 ####################################
 model=keras.Sequential()
 model.add(layers.Dense(1,input_dim=1))
-model.add(layers.Dense(2,input_dim=3))
+model.add(layers.Dense(2,input_dim=1))
 model.add(layers.Dense(3,input_dim=1))
+model.add(layers.Dense(4,input_dim=1))
 model.summary()
 ######################################
-model.compile(optimizer="sdg", loss="mse")
+model.compile(optimizer="adam", loss="mse")
 #######################################
-model.fit(Axis_x,Axis_y,epochs=10000,batch_size=150)
-model.predict(Axis_x)
+model.fit(Axis_x,Axis_y,epochs=3000,batch_size=150)
 plt.plot(Axis_x,model.predict(Axis_x))
-plt.scatter(Axis_x,model.predict(Axis_x))
+plt.show()
+#####################################
+plt.scatter(Axis_x,Axis_y)
+x=model.predict(Axis_x)
+y=model.predict(Axis_y)
+plt.scatter(x, y)
 plt.show()
 
 
