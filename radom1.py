b18820a48ac04bb26b2fd3efcc17e33c2a0c1a05
TerryPanda3032 <terrypch@163.com>
2021-08-08 14:58:14 +0800

    更新 radom1.py

--- radom1.py
+++ radom1.py
@@ -7,24 +7,26 @@ import numpy as np
 from keras import regularizers
 ####################################
 print(" import==100%")
-Axis_x=np.linspace(0, 100,30)
+Axis_x=np.linspace(0, 100,100)
 Axis_y=0
-Axis_y=3*Axis_x + 6 + np.random.randn(30) * 7
+Axis_y=3*Axis_x + 6 + np.random.randn(100) * 7
 print(Axis_x,Axis_y)
 plt.plot(Axis_x,Axis_y)
 ####################################
 model=keras.Sequential()
-model.add(layers.Dense(1,input_dim=1,activity_regularizer=regularizers.l2(0.03)))
+model.add(layers.Dense(1,input_dim=1))
 model.summary()
 ######################################
 model.compile(optimizer="adam", loss="mse")
 #######################################
-model.fit(Axis_x,Axis_y,epochs=3000,batch_size=150)
+model.fit(Axis_x,Axis_y,epochs=8000)
 #####################################
 plt.plot(Axis_x,Axis_y)
+plt.scatter(Axis_x,Axis_y)
 x=model.predict(Axis_x)
 y=model.predict(Axis_y)
 plt.plot(x, y)
+plt.scatter(x,y)
 plt.show()
 a=model.predict([100])
 plt.scatter(a, a)
